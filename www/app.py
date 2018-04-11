446c001280f652529adda24959cccab5cfd4894f
张炎 <970730254@qq.com>
2018-04-12 00:09:57 +0800

    Day-2: create .gitnore and app.py

---
+++ www/app.py
@@ -0,0 +1,30 @@
+# _*_ coding: utf-8 _*_
+
+__author__ = 'Morgan'
+
+'''
+async web application.
+'''
+
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from  datetime import  datetime
+from  aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>')
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('Server started at http://127.0.0.1:9000')
+    return  srv
+
+#获取 eventLoop
+loop = asyncio.get_event_loop()
+#执行coroutine
+loop.run_until_complete(init(loop))
+loop.run_forever()
